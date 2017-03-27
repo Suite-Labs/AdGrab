700d12b998b786f8131c3eafb8addb71010345b1
Seanking11 <sean.king0811@gmail.com>
2017-03-27 19:16:39 -0400

    Added Contact Modal
    Stores data in object called consumer

--- js/site.js
+++ js/site.js
@@ -3,3 +3,27 @@ $(".section-two-zoom").click(function () {
         scrollTop: $(".section-two").offset().top
     }, 500);
 });
+
+$(document).ready(function() {
+  $('.modal-trigger').leanModal();
+});
+
+var Customer = (function() {
+
+    return function Customer(firstName, lastName, email, message) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.message = message;
+    }
+
+})();
+
+$('#contactBtn').on('click', function(){
+    var f = $('#inputFirstName').val(),
+        l = $('#inputLastName').val(),
+        e = $('#email').val(),
+        m = $('#inputTextArea').val();
+    var contact = new Customer(f, l, e, m);
+    console.log(contact);
+});
